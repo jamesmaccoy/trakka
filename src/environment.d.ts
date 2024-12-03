b9cfda957463e9ec59db39f21fea084c01571f08
jamesmaccoy <thankyou.digital@gmail.com>
2024-12-03 11:28:04 +0200

    logo

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PAYLOAD_SECRET: string
+      PAYLOAD_SECRET: 56c81ae-0fda-4ad5-ac2c-cdd8e1588d4c
       DATABASE_URI: string
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
