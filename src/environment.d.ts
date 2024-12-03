23bf5faac71f4464986432a840e1be6a3b2bff7d
jamesmaccoy <thankyou.digital@gmail.com>
2024-12-03 11:32:00 +0200

    env sting

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PAYLOAD_SECRET: 56c81ae-0fda-4ad5-ac2c-cdd8e1588d4c
+      PAYLOAD_SECRET: string
       DATABASE_URI: string
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
@@ -12,3 +12,4 @@ declare global {
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
 export {}
+ß
